e33e4f7cc0dbdbd63b876602aeba81daba9b80d0
Louise Verbeke <louiseverbeke@yahoo.be>
2026-03-22 18:08:46 -0400

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Louise Verbeke on 22/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
